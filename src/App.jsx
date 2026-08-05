7a65d2afd967f96fda97bb709b7dcf661f1050a1
meenamurali1217-dot <meena.murali1217@gmail.com>
2026-08-05 12:02:08 +0530

    upadete root path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import WhyShineEx from './pages/WhyShineEx';
-import ProductsPage from './pages/ProductsPage';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import WhyShineEx from "./pages/WhyShineEx";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
   return (
-    <Router basename="/shineEx">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
