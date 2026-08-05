854012513ab782bc4e66c9c76a0f5950d2118a15
meenamurali1217-dot <meena.murali1217@gmail.com>
2026-08-05 11:04:48 +0530

    Fix BrowserRouter basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -7,7 +7,7 @@ import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/shineEx">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
